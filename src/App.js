2657c1262645659d8197acf72b034deed82adc1a
kennyalmendral <kennyalmendral@gmail.com>
2021-04-29 06:23:04 +0800

    Update UI

--- src/App.js
+++ src/App.js
@@ -31,21 +31,49 @@ function App() {
   }
 
   return (
-    <>
-      <input type="text" onChange={searchTitle} value={query} />
+    <div className="app">
+      <div className="card">
+        <div className="card-header">
+          <h4>React Infinite Scroll</h4>
+        </div>
 
-      {books.map((book, index) => {
-        {/* Get reference to the very last book */}
-        if (books.length === index + 1) {
-          return <div ref={lastBookElementRef} key={book}>{book}</div>
-        } else {
-          return <div key={book}>{book}</div>
-        }
-      })}
+        <div className="card-body">
+          <label htmlFor="keyword">Enter keyword</label>
+          <input type="text" className="form-control" id="keyword" name="keyword" onChange={searchTitle} value={query} />
 
-      <div>{loading && 'Loading...'}</div>
-      <div>{error && 'Error'}</div>
-    </>
+          <div className="books-container">
+            <h4>Results</h4>
+            
+            {books.length > 0 && (
+              <div>
+                {books.map((book, index) => {
+                  // eslint-disable-next-line no-lone-blocks
+                  {/* Get reference to the very last book */}
+                  if (books.length === index + 1) {
+                    return <div ref={lastBookElementRef} key={book}>{book}</div>
+                  } else {
+                    return <div key={book}>{book}</div>
+                  }
+                })}
+
+                {loading && (
+                  <div className="loader text-muted">
+                    <i className="fa fa-spinner fa-spin" />
+                    <span>Loading...</span>
+                  </div>
+                )}
+
+                {error && (
+                  <div>Something went wrong.</div>
+                )}
+              </div>
+            )}
+
+            {books.length === 0 && <div>No results found</div>}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
